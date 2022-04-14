8a1698bc10f8cf8622e64cf3b46a3f65b09064c2
antoniapavin <99196704+antoniapavin@users.noreply.github.com>
2022-04-14 11:43:13 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script></script>
+
+<template>
+  <header>
+    <h1>clique aqui</h1>
+  </header>
+  <button class="btnClique">aqui</button>
+</template>
+
+<style>
+body {
+  text-align: center;
+  margin: 300px;
+}
+h1 {
+  text-align: center;
+  color: red;
+  text-decoration: underline rgb(241, 131, 177);
+}
+</style>
